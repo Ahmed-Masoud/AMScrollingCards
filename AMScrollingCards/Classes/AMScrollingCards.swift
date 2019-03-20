5b5a7872a992e6c8c4c54f0e8b46f75854d1f206
Ahmed-Masoud <ahmed_ehab200@hotmail.com>
2019-03-20 13:16:25 +0200

    [EXPORT] page indicator tint colors

--- AMScrollingCards/Classes/AMScrollingCards.swift
+++ AMScrollingCards/Classes/AMScrollingCards.swift
@@ -24,6 +24,8 @@ extension SwipingCardsManagerDelegate {
 public final class SwipingCardsManager: NSObject {
     
     public var cardsView: UIView!
+    public var pageDotColor: UIColor = UIColor(hexString: "#E2E2E2")
+    public var selectedPageDotColor: UIColor = UIColor(hexString: "#929292")
     private var collectionView: UICollectionView!
     private var collectionViewLayout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
     private var numberOfItems: Int = 0
@@ -50,6 +52,8 @@ public final class SwipingCardsManager: NSObject {
         pageControl = UIPageControl(frame: CGRect(x: 0, y: 0, width: frame.width, height: 33))
         pageControl.numberOfPages = numberOfItems
         pageControl.isUserInteractionEnabled = false
+        pageControl.pageIndicatorTintColor = pageDotColor
+        pageControl.currentPageIndicatorTintColor = selectedPageDotColor
         setupPageControlConstraints()
     }
     
@@ -179,3 +183,31 @@ extension SwipingCardsManager: UICollectionViewDataSource, UICollectionViewDeleg
 
 
 
+extension UIColor {
+    convenience init(hexString: String, alpha: CGFloat = 1.0) {
+        let hexString: String = hexString.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        let scanner = Scanner(string: hexString)
+        if (hexString.hasPrefix("#")) {
+            scanner.scanLocation = 1
+        }
+        var color: UInt32 = 0
+        scanner.scanHexInt32(&color)
+        let mask = 0x000000FF
+        let r = Int(color >> 16) & mask
+        let g = Int(color >> 8) & mask
+        let b = Int(color) & mask
+        let red   = CGFloat(r) / 255.0
+        let green = CGFloat(g) / 255.0
+        let blue  = CGFloat(b) / 255.0
+        self.init(red:red, green:green, blue:blue, alpha:alpha)
+    }
+    func toHexString() -> String {
+        var r:CGFloat = 0
+        var g:CGFloat = 0
+        var b:CGFloat = 0
+        var a:CGFloat = 0
+        getRed(&r, green: &g, blue: &b, alpha: &a)
+        let rgb:Int = (Int)(r*255)<<16 | (Int)(g*255)<<8 | (Int)(b*255)<<0
+        return String(format:"#%06x", rgb)
+    }
+}
